e885c90d3e4ad9014dd51d8e04b973f1bcd7f8aa
NguyenTrongNhan1707 <nguyentrongnhan17072005@gmail.com>
2026-03-14 12:16:47 +0700

    Hoan thanh bai1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoten: Nguyen Trong Nhan");
+            Console.WriteLine("MSV: 23115053122125");
+            Console.WriteLine("Bai tap 1: tao danh sach so nguyen");
+            List<int> listInt = new List<int>();
+            listInt.Add(205);
+            listInt.Add(206);
+            listInt.Add(1707);
+            listInt.Add(113);
+            listInt.Add(3012);
+            Console.WriteLine("Danh sach so nguyen: ");
+            foreach (int i in listInt)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
