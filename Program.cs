db6679d5fae329522db3b43dbca6448403f3acc9
NguyenTrongNhan1707 <nguyentrongnhan17072005@gmail.com>
2026-03-16 15:21:58 +0700

    Hoan thanh bai 10

--- Program.cs
+++ Program.cs
@@ -12,20 +12,23 @@ namespace GenericCollection
         {
             Console.WriteLine("Hoten: Nguyen Trong Nhan");
             Console.WriteLine("MSV: 23115053122125");
-            Console.WriteLine("Bai tap 9:Loai bo cac phan tu trung");
-            List<int> list = new List<int> { 1, 2, 3, 4, 5, 5, 9, 9, 15, 40 };
-            Console.WriteLine("Danh sach ban dau:");
-            foreach (int item in list)
+            Console.WriteLine("Bai tap 10: Tim phan tu nho nhat");
+            List<int> ints = new List<int>() { 20, 11, 7, 67, 59 };
+            Console.WriteLine("Danh sach cac phan tu: ");
+            foreach (int i in ints)
             {
-                Console.Write(item + " ");
+                Console.Write(i + " ");
             }
-            Console.WriteLine("\nDanh sach sau khi loai bo cac phan tu trung:");
-            List<int> uniqueList = list.Distinct().ToList();
-            foreach (int item in uniqueList)
+            int min = ints[0];
+            for (int i = 1; i < ints.Count; i++)
             {
-                Console.Write(item + " ");
+                if (ints[i] < min)
+                {
+                    min = ints[i];
+                }
             }
-            Console.ReadLine();
+            Console.WriteLine("\nPhan tu nho nhat la: " + min);
+
         }
         }
 }
