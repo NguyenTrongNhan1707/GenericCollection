223e7d86818f482ff25de3b582a5a1822ea6fca7
NguyenTrongNhan1707 <nguyentrongnhan17072005@gmail.com>
2026-03-16 17:30:44 +0700

    Hoan thanh bai 15

--- Program.cs
+++ Program.cs
@@ -18,37 +18,30 @@ namespace GenericCollection
         {
             Console.WriteLine("Hoten: Nguyen Trong Nhan");
             Console.WriteLine("MSV: 23115053122125");
-            Console.WriteLine("Bai tap 14: Xoa sinh vien theo ID");
-            List<student> students = new List<student>();
-            students.Add(new student() { id = 17, name = "Nguyen Trong Nhan" });
-            students.Add(new student() { id = 21, name = "Do Ngoc Hoang Anh" });
-            students.Add(new student() { id = 30, name = "Nguyen Long Nhat" });
-            students.Add(new student() { id = 07, name = "Nguyen Long Nha" });
-            students.Add(new student() { id = 01, name = "Nguyen Thanh Cong" });
-            students.Add(new student() { id = 50, name = "Pham Van Vu" });
-            Console.WriteLine("Danh sach sinh vien: ");
-            foreach (student s in students)
+            Console.WriteLine("Bai tap 15: Su dung Dictionary");
+            Dictionary<String, string> dict = new Dictionary<String, string>
             {
-                Console.WriteLine("ID: {0}, Name: {1}", s.id, s.name);
-
-            }
-            Console.WriteLine("Nhap ID sinh vien can xoa: ");
-            int id = int.Parse(Console.ReadLine());
-            student studentToRemove = students.FirstOrDefault(s => s.id == id);
-            if (studentToRemove != null)
+                {"hello", "xin chao"},
+                {"goodbye", "tam biet"},
+                {"thank you", "cam on"},
+                {"sorry", "xin loi"}
+            };
+            Console.WriteLine("Tu dien hien tai co cac tu sau:");
+            foreach (var item in dict)
             {
-                students.Remove(studentToRemove);
-                Console.WriteLine("Da xoa sinh vien co ID: {0}", id);
+                Console.WriteLine(item.Key);
             }
-            else
-            {
-                Console.WriteLine("Khong tim thay sinh vien co ID: {0}", id);
+                Console.WriteLine("Nhap tu can dich:");
+                string input = Console.ReadLine();
+                if (dict.ContainsKey(input))
+                {
+                    Console.WriteLine("Nghia cua tu '{0}' la: {1}", input, dict[input]);
+                }
+                else
+                {
+                    Console.WriteLine("Tu '{0}' khong co trong tu dien.", input);
             }
-            Console.WriteLine("Danh sach sinh vien sau khi xoa: ");
-            foreach (student s in students)
-            {
-                Console.WriteLine("ID: {0}, Name: {1}", s.id, s.name);
-            }
+                Console.ReadLine();
         }
     }
 }
