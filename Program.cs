5e688df38e4330149178d815f61c3a7f06f31a19
NguyenTrongNhan1707 <nguyentrongnhan17072005@gmail.com>
2026-03-16 14:52:10 +0700

    Hoan thanh bai 9

--- Program.cs
+++ Program.cs
@@ -12,21 +12,20 @@ namespace GenericCollection
         {
             Console.WriteLine("Hoten: Nguyen Trong Nhan");
             Console.WriteLine("MSV: 23115053122125");
-            Console.WriteLine("Bai tap 8: Dao nguoc danh sach");
-            List<int> list = new List<int> { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Bai tap 9:Loai bo cac phan tu trung");
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 5, 9, 9, 15, 40 };
             Console.WriteLine("Danh sach ban dau:");
             foreach (int item in list)
             {
                 Console.Write(item + " ");
             }
-            Console.WriteLine();
-            list.Reverse();
-            Console.WriteLine("Danh sach sau khi dao nguoc:");
-            foreach (int item in list)
+            Console.WriteLine("\nDanh sach sau khi loai bo cac phan tu trung:");
+            List<int> uniqueList = list.Distinct().ToList();
+            foreach (int item in uniqueList)
             {
                 Console.Write(item + " ");
             }
-            Console.WriteLine();
+            Console.ReadLine();
         }
         }
 }
