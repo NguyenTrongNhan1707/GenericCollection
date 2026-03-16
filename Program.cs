3cb26708b075932a72c4f975fb45fac633cee9df
NguyenTrongNhan1707 <nguyentrongnhan17072005@gmail.com>
2026-03-16 17:48:41 +0700

    Hoan thanh Bai18 cuoi cung

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace GenericCollection
 {
     class student
     {
-        public int id;
         public string name;
+        public double score;
     }
     internal class Program
     {
@@ -18,21 +18,26 @@ namespace GenericCollection
         {
             Console.WriteLine("Hoten: Nguyen Trong Nhan");
             Console.WriteLine("MSV: 23115053122125");
-            Console.WriteLine("Bai tap 17: Tim phan tu xuat hien nhieu nhat");
-            List<int> ints = new List<int>();
-            Console.WriteLine("Nhap so luong phan tu: ");
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 0; i < n; i++)
+            Console.WriteLine("Bai tap 18: Quan ly diem sinh vien");
+            List<student> students = new List<student>();
+            students.Add(new student() { name = "Nguyen Van Luy", score = 8.5 });
+            students.Add(new student() { name = "Nguyen Van Tam", score = 7.0 });
+            students.Add(new student() { name = "Tran Cao Van", score = 9.0 });
+            students.Add(new student() { name = "Tran Van Viet", score = 9.0 });
+            Console.WriteLine("Danh sach sinh vien:");
+            foreach (var student in students)
             {
-                Console.WriteLine("Nhap phan tu thu " + (i + 1) + ": ");
-                int x = int.Parse(Console.ReadLine());
-                ints.Add(x);
+                Console.WriteLine($"Ten: {student.name}, Diem: {student.score}");
             }
-            var mostFrequent = ints.GroupBy(x => x)
-                                   .OrderByDescending(g => g.Count())
-                                   .First()
-                                   .Key;
-            Console.WriteLine("Phan tu xuat hien nhieu nhat: " + mostFrequent + " So lan xuat hien: " + ints.Count(x => x == mostFrequent));
+            Console.WriteLine("Sinh vien có diem cao nhat la");
+            double maxScore = students.Max(s => s.score);
+            var topStudents = students.Where(s => s.score == maxScore).ToList();
+
+            Console.WriteLine("\nSinh vien co diem cao nhat la:");
+            foreach (var s in topStudents)
+            {
+                Console.WriteLine($"Ten: {s.name}, Diem: {s.score}");
+            }
             Console.ReadLine();
         }
     }
