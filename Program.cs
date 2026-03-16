2f7018c543a20e5e87e012a836b0b28f2d648d92
NguyenTrongNhan1707 <nguyentrongnhan17072005@gmail.com>
2026-03-16 14:48:35 +0700

    Hoan thanh bai 8

--- Program.cs
+++ Program.cs
@@ -12,28 +12,21 @@ namespace GenericCollection
         {
             Console.WriteLine("Hoten: Nguyen Trong Nhan");
             Console.WriteLine("MSV: 23115053122125");
-            Console.WriteLine("Bai tap 7: Sap xep danh sach");
-            List<int> numbers = new List<int>();
-            Console.WriteLine("Nhap so luong phan tu: ");
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 0; i < n; i++)
+            Console.WriteLine("Bai tap 8: Dao nguoc danh sach");
+            List<int> list = new List<int> { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Danh sach ban dau:");
+            foreach (int item in list)
             {
-                Console.WriteLine("Nhap phan tu thu {0}: ", i + 1);
-                int number = int.Parse(Console.ReadLine());
-                numbers.Add(number);
+                Console.Write(item + " ");
             }
-            Console.WriteLine("\nDanh sach truoc khi sap xep: ");
-            foreach (int number in numbers)
+            Console.WriteLine();
+            list.Reverse();
+            Console.WriteLine("Danh sach sau khi dao nguoc:");
+            foreach (int item in list)
             {
-                Console.Write(number + " ");
+                Console.Write(item + " ");
             }
-            Console.WriteLine("\nDanh sach sau khi sap xep: ");
-            numbers.Sort();
-            foreach (int number in numbers)
-            {
-                Console.Write(number + " ");
-            }
-            Console.ReadLine();
+            Console.WriteLine();
         }
         }
 }
